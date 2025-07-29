39d9e49f190b6c815553344919cd0503d4eb9622
yusufulku <yusuf.ulku@eba.gov.tr>
2025-07-29 18:14:45 +0300

    Spring security OAUTH2 basic social login initial commit

---
+++ src/main/java/com/imerasoft/springsecoauth2/controller/secureController.java
@@ -0,0 +1,5 @@
+package com.imerasoft.springsecoauth2.controller;
+
+/* Created by yusufulku,29.07.2025 */
+public class secureController {
+}
---
+++ src/test/java/com/imerasoft/springsecoauth2/SpringsecOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imerasoft.springsecoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
