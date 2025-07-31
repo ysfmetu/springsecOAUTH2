fea490b2faf9617905d81d24af8ba423a365bd2b
yusufulku <yusuf.ulku@eba.gov.tr>
2025-07-31 16:50:57 +0300

    social login için security config ayarları öncelikle config dosyası oluşturuldu secure diye bir html page oluşturuldu authenticated işlemi ile bu kısmı login işlemine zorladık github ve facebook için config ayarı yapıldı

---
+++ src/main/java/com/imerasoft/springsecoauth2/config/ProjectSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.imerasoft.springsecoauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+/* Created by yusufulku,31.07.2025 */
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((requests) -> requests.requestMatchers("/secure").authenticated()
+                .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+    @Bean
+    ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration githubClient = githubClientRegistration();
+        ClientRegistration facebookClient = facebookClientRegistration();
+        return new InMemoryClientRegistrationRepository(githubClient,facebookClient);
+    }
+
+    private ClientRegistration githubClientRegistration() {
+        return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("clientId").clientSecret("clientSecret")
+                .scope("read:user").build();
+    }
+    private ClientRegistration facebookClientRegistration() {
+        return CommonOAuth2Provider.FACEBOOK.getBuilder("facebook").clientId("clientId").clientSecret("clientSecret")
+                .scope("read:user").build();
+    }
+}
--- src/main/java/com/imerasoft/springsecoauth2/controller/secureController.java
+++ src/main/java/com/imerasoft/springsecoauth2/controller/secureController.java
@@ -1,5 +1,13 @@
 package com.imerasoft.springsecoauth2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /* Created by yusufulku,29.07.2025 */
+@Controller
 public class secureController {
+    @GetMapping("/secure")
+    public String securePage() {
+        return "secure.html";
+    }
 }
